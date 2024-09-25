6df696084e41f9064e45d3bb590c6e28dfb7c76f
Solution4904 <shinjseoup@gmail.com>
2024-09-25 19:52:57 +0900

    # 메모 작성
    - 레이아웃 생성 (ing...)
    - 기능 작성 (ing...)

--- app/src/main/java/app/solution/swing_by/MainActivity.kt
+++ app/src/main/java/app/solution/swing_by/MainActivity.kt
@@ -1,20 +1,31 @@
 package app.solution.swing_by
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import app.solution.swing_by.databinding.ActivityMainBinding
+import app.solution.swing_by.feature.WriteMemoActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val intent = Intent(this, WriteMemoActivity::class.java)
+        startActivity(intent)
+
+//        enableEdgeToEdge()
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
---
+++ app/src/main/java/app/solution/swing_by/feature/WriteMemoActivity.kt
@@ -0,0 +1,41 @@
+package app.solution.swing_by.feature
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import app.solution.swing_by.databinding.ActivityWriteMemoBinding
+import com.google.android.material.chip.Chip
+
+class WriteMemoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWriteMemoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityWriteMemoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setChipButtons()
+    }
+
+    private fun setChipButtons() {
+        fun checkedAndClearing(view: View) {
+            binding.chipGroup.clearCheck()
+
+            val chip = view as Chip
+            chip.isChecked = true
+        }
+
+        with(binding) {
+            chipBank.setOnClickListener { checkedAndClearing(it) }
+            chipMart.setOnClickListener { checkedAndClearing(it) }
+            chipMiscellaneous.setOnClickListener { checkedAndClearing(it) }
+            chipClothes.setOnClickListener { checkedAndClearing(it) }
+            chipErrands.setOnClickListener { checkedAndClearing(it) }
+            chipRestaurant.setOnClickListener { checkedAndClearing(it) }
+            chipCosmetic.setOnClickListener { checkedAndClearing(it) }
+            chipEtc.setOnClickListener { checkedAndClearing(it) }
+        }
+    }
+}
