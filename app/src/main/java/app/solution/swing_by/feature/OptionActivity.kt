1608e1739ec42897515abebaf58e53e16eb91c86
Solution4904 <shinjseoup@gmail.com>
2024-11-13 19:39:56 +0900

    # 옵션
    - 메모 리스트 상단에 옵션으로 진입하기 위한 액션바 추가

---
+++ app/src/main/java/app/solution/swing_by/feature/OptionActivity.kt
@@ -0,0 +1,14 @@
+package app.solution.swing_by.feature
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import app.solution.swing_by.R
+import app.solution.swing_by.base.BaseActivity
+import app.solution.swing_by.databinding.ActivityOptionBinding
+
+class OptionActivity : BaseActivity<ActivityOptionBinding>(ActivityOptionBinding::inflate) {
+
+}
